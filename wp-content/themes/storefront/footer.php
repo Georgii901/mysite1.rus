3ea35cd02f30e940f7391c8026dd650674fc0388
Evgeniy <48757537+EvgehaName@users.noreply.github.com>
2021-12-25 11:42:47 +0300

    Done

---
+++ wp-content/themes/storefront/footer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+
+?>
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+	<?php do_action( 'storefront_before_footer' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="col-full">
+
+			<?php
+			/**
+			 * Functions hooked in to storefront_footer action
+			 *
+			 * @hooked storefront_footer_widgets - 10
+			 * @hooked storefront_credit         - 20
+			 */
+			do_action( 'storefront_footer' );
+			?>
+			
+			<div class="site-info info-custom">
+			© ООО "Пятёрочка" 2021
+							<br>
+				<a href="http://mysite1.rus/wp-content/uploads/2021/12/privacy_policy_agrotorg.pdf" target="_blank" title="Условия использования интернет-магазина" rel="noreferrer">Условия использования</a>.					</div>
+
+		</div><!-- .col-full -->
+	</footer><!-- #colophon -->
+
+	<?php do_action( 'storefront_after_footer' ); ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
